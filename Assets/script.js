5f7c53e3775bb06e672773a0df9fc04b876d1204
PaulRusz <paulruszkay@gmail.com>
2024-02-25 17:03:26 -0500

    added API key

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,7 @@
 
+var apiKey = "98d710491c449ad5a6ba81a14f1ff914"
+let city;
+
 
 var currentDate = dayjs().format('dddd, MMMM D, YYYY, h:mm a')
 var displayDate = document.querySelector('currentDate')
